dff072370e460c3644b17c77fa33c2e26bb9e9e9
Mastersam <abadasamuelosp@gmail.com>
2019-12-30 01:47:33 -0800

    fixed like feature

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: new Scaffold(
-        appBar: new AppBar(
-          title: Text('Wordy'),
-          actions: <Widget>[
-            IconButton(
-                icon: new Icon(Icons.more_vert),
-                onPressed: (){}
-                )
-          ],
-        ),
-        body: RandomSentences()
-      ),
+          appBar: new AppBar(
+            title: Text('Wordy'),
+            actions: <Widget>[
+              IconButton(icon: new Icon(Icons.more_vert), onPressed: () {})
+            ],
+          ),
+          body: RandomSentences()),
     );
   }
 }
@@ -35,70 +31,66 @@ class RandomSentences extends StatefulWidget {
 }
 
 class RandomSentencesState extends State<RandomSentences> {
+  final _sentences = <String>[];
+  final _funnies = new Set<String>();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-
-    final _sentences = <String>[];
-    final _funnies = new Set<String>();
-
-    String _getSentence() {
-      final noun = new WordNoun.random();
-      final adjective = new WordAdjective.random();
-      return 'The developer wrote a ${adjective.asLowerCase} app in flutter and'
-          ' showed it to his ${noun.asLowerCase}';
-    }
-
-    Widget _buildRow(String sentence) {
-      final alreadyfunny = _funnies.contains(sentence);
-      return new ListTile(
-        title: new Text(
-          sentence,
-          style: TextStyle(fontSize: 14.0),
-        ),
-        trailing: new Icon(
-          alreadyfunny ? Icons.thumb_down : Icons.thumb_up,
-          color: alreadyfunny ? Colors.green : null,
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadyfunny) {
-              _funnies.remove(sentence);
-            }
-            else {
-              _funnies.add(sentence);
-            }
-          });
-        },
-      );
-    }
-
-
-    Widget _buildSentences() {
-      return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-// An ItemBuilder callback is used to add sentences and dividers
-// based on when the user scrolls down the view.
-        itemBuilder: (context, i) {
-// Adds a pixel divider before each row
-          if (i.isOdd) return new Divider();
-// Divides i by 2 then returns the value into index
-// This counts how many sentences are in the ListView
-          final index = i ~/ 2;
-// Checks to see if we've hit the end of the sentence list
-          if (index >= _sentences.length) {
-// If we have then we generate another 10
-            for (int x = 0; x < 10; x++) {
-              _sentences.add(_getSentence());
-            }
-          }
-          return _buildRow(_sentences[index]);
-        },
-      );
-    }
-
     return new Scaffold(
       body: _buildSentences(),
+    );
+  }
+
+  String _getSentence() {
+    final noun = new WordNoun.random();
+    final adjective = new WordAdjective.random();
+    return 'The developer wrote a ${adjective.asLowerCase} app in flutter and'
+        ' showed it to his ${noun.asLowerCase}';
+  }
+
+  Widget _buildRow(String sentence) {
+    final alreadyfunny = _funnies.contains(sentence);
+    return new ListTile(
+      title: new Text(
+        sentence,
+        style: TextStyle(fontSize: 14.0),
+      ),
+      trailing: new Icon(
+        alreadyfunny ? Icons.thumb_up : Icons.thumb_down,
+        color: alreadyfunny ? Colors.green : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadyfunny) {
+            _funnies.remove(sentence);
+          } else {
+            _funnies.add(sentence);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSentences() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+// An ItemBuilder callback is used to add sentences and dividers
+// based on when the user scrolls down the view.
+      itemBuilder: (context, i) {
+// Adds a pixel divider before each row
+        if (i.isOdd) return new Divider();
+// Divides i by 2 then returns the value into index
+// This counts how many sentences are in the ListView
+        final index = i ~/ 2;
+// Checks to see if we've hit the end of the sentence list
+        if (index >= _sentences.length) {
+// If we have then we generate another 10
+          for (int x = 0; x < 10; x++) {
+            _sentences.add(_getSentence());
+          }
+        }
+        return _buildRow(_sentences[index]);
+      },
     );
   }
 }
